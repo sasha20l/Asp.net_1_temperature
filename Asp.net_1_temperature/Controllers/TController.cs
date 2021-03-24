b20edaf948f48f73ec186b4a09be3c40098d3f51
Sasha21l <sasha21l@mail.ru>
2021-03-24 22:19:42 +0500

    Добавьте файлы проекта.

---
+++ Asp.net_1_temperature/Controllers/TController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.net_1_temperature.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TController : ControllerBase
+    {
+        private readonly Temperature holder;
+
+        public TController(Temperature holder)
+        {
+            this.holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] int input)
+        {
+            holder.CreateTemperature(input);
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(holder.temperatureObject);
+        }
+
+        //[HttpPut("update")]
+        //public IActionResult Update([FromQuery] string stringsToUpdate, [FromQuery] string newValue)
+        //{
+        //    //for (int i = 0; i < holder.Values.Count; i++)
+        //    //{
+        //    //    if (holder.Values[i] == stringsToUpdate)
+        //    //        holder.Values[i] = newValue;
+        //    //}
+
+        //    //return Ok();
+        //}
+
+        //[HttpDelete("delete")]
+        //public IActionResult Delete([FromQuery] string stringsToDelete)
+        //{
+        //    //holder.Values = holder.Values.Where(w => w != stringsToDelete).ToList();
+        //    //return Ok();
+        //}
+
+
+
+
+    }
+}
---
+++ Asp.net_1_temperature/Temperature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.net_1_temperature
+{
+    public class Temperature
+    {
+        public List<TemperatureObject> temperatureObject = new List<TemperatureObject>();
+        public void CreateTemperature(int temperature)
+        {
+            temperatureObject.Add(new TemperatureObject(temperature, DateTime.Now));
+        }
+    }
+
+    public class TemperatureObject
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public TemperatureObject(int temperatureC, DateTime date) { TemperatureC = temperatureC; Date = date; }
+
+
+    }
+}
